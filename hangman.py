0108a651952ce8ce323be31f9131d99fcc88c61e
deinosuch <pasekaadam@gmail.com>
2023-02-05 02:56:43 +0100

    game done

--- hangman.py
+++ hangman.py
@@ -4,3 +4,26 @@ import random
 with open("words.txt", "r") as f:
     words = f.read().splitlines()
     
+word = random.choice(words)
+
+knowledge = ['_'] * len(word)
+lives = 10
+
+while '_' in knowledge:
+    if lives == 0:
+        print('Game over')
+        break
+    
+    print(f'You have {lives} lives')
+    print(*knowledge)
+    guess = input()
+    if guess not in word:
+        lives -= 1
+        continue
+
+    for i, x in enumerate(word):
+        if x != guess:
+            continue
+        knowledge[i] = guess
+else:
+    print('YIPIIEEEEE, you won! Congrats :)')
